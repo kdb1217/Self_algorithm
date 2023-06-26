6521099127af68bd20f055072aca9245690b9726
김다빈 <84852135+kdb1217@users.noreply.github.com>
2023-06-27 00:53:32 +0900

    [unrated] Title: 덧칠하기, Time: 170.17 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,15 +1,17 @@
 def solution(n, m, section):
     answer = 0
-    
-
-    while(len(section) > 0):
-        current = section[0] 
-        for i in range(m): 
-            if current == section[0]: 
-                section.pop(0) 
-                if len(section) == 0: 
-                    break
-            current += 1
+    # 모든 영역을 다 칠할 때 까지 반복
+    while section:
+        # 윈도우 설정
+        end = section[-1] - 1
+        start = end - m + 1
+        # 범위 초과 경우
+        if start < 0:
+            start, end = 0, m - 1
         answer += 1
-        
+        while start <= (section[-1] - 1):
+            section.pop()
+            # 다 제거한 경우
+            if not section:
+                break
     return answer
